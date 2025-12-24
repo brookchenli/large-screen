51ef1d93cfed518212c00ef4bc9be141c2ce7427
陈历 <chenli@rongcloud.cn>
2025-12-24 22:49:42 +0800

    feat:屏幕分辨率适配

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
 import MainScreen from "./components/main-screen";
 import ViewManager from "./components/view-manager";
-
+import TestMain from "./components/test-main";
 function App() {
   return (
-    <ViewManager width={3784} height={1376}>
+    <ViewManager width={4800} height={2080}>
       <MainScreen />
     </ViewManager>
   );
---
+++ src/components/test-main/index.tsx
@@ -0,0 +1,12 @@
+import "./index.less";
+export default function TestMain() {
+  return (
+    <div className="test-main">
+      <div className="test-main-header"></div>
+      <div className="test-main-content">
+        <div className="test-main-content-left">11</div>
+        <div className="test-main-content-right">22</div>
+      </div>
+    </div>
+  );
+}
